1e37b17eb858fd5cee84b0e843daefe27a218bdb
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-04-15 16:59:44 -0300

    Lesson 50

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
+import {
+  messagesQuery,
+  addMessageMutation,
+  messageAddedSubscription,
+} from './graphql/queries';
+import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
 const Chat = ({ user }) => {
   const [messages, setMessages] = useState([]);
+  useQuery(messagesQuery, {
+    onCompleted: ({ messages }) => setMessages(messages),
+  });
 
-  const handleSend = (text) =>
-    setMessages(messages.concat({ id: text, from: 'you', text }));
+  useSubscription(messageAddedSubscription, {
+    onSubscriptionData: ({ subscriptionData }) =>
+      setMessages(messages.concat(subscriptionData.data.messageAdded)),
+  });
+
+  const [addMessage] = useMutation(addMessageMutation);
+
+  const handleSend = (text) => addMessage({ variables: { input: { text } } });
 
   return (
     <section className="section">
